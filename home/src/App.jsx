5092785040dcb0a463016d8a1bc18c9dae095d31
v.shatilov <v.shatilov@tinkoff.ru>
2022-10-04 02:54:49 +0300

    adding direct product page NOT WORKING

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -4,14 +4,12 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import HomeContent from "./HomeContent";
 
 const App = () => (
   <div className="text-3xl mx-auto max-w-6xl">
     <Header/>
-        <div className="my-10">
-            Home Page Content
-        </div>
+      <HomeContent/>
     <Footer/>
   </div>
 );
