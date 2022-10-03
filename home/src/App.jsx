b057a494fd19423d8541c627a5d81205fb7ac79a
v.shatilov <v.shatilov@tinkoff.ru>
2022-10-03 16:26:59 +0300

    add Header and Footer to home

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header/>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Footer/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
